3f54646d3e4aeacb22ff9d76dfa9765a467ac105
Park Jae-Min <dhkdhkdhkdn@gmail.com>
2024-04-08 18:32:52 +0900

    Build : 기존 OAuth, Security 파일 삭제

--- Server/src/main/java/ForMZ/Server/global/auth/oauth2/controller/OAuthController.java
+++
@@ -1,4 +0,0 @@
-package ForMZ.Server.global.auth.oauth2.controller;
-
-public class OAuthController {
-}
--- Server/src/main/java/ForMZ/Server/global/auth/oauth2/dto/PhonePostReq.java
+++
@@ -1,4 +0,0 @@
-package ForMZ.Server.global.auth.oauth2.dto;
-
-public class PhonePostReq {
-}
