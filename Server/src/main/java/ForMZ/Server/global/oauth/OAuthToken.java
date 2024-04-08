e18a903238b206b12777537dc2a67f9931984361
Park Jae-Min <dhkdhkdhkdn@gmail.com>
2024-04-08 18:36:40 +0900

    Feat : OAuthProperty 설정 추가

---
+++ Server/src/main/java/ForMZ/Server/global/oauth/OAuthToken.java
@@ -0,0 +1,13 @@
+package ForMZ.Server.global.oauth;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class OAuthToken {
+    private String accessToken;
+}
