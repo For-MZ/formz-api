daf228f0958cb23ca62f4c726be3bf8350eee240
Park Jae-Min <dhkdhkdhkdn@gmail.com>
2024-04-08 18:38:35 +0900

    Feat : OAuth Exception 추가

---
+++ Server/src/main/java/ForMZ/Server/global/oauth/AuthorizationCodeErrorException.java
@@ -0,0 +1,14 @@
+package ForMZ.Server.global.oauth;
+
+import ForMZ.Server.global.exception.ApplicationException;
+import org.springframework.http.HttpStatus;
+
+public class AuthorizationCodeErrorException extends ApplicationException {
+    public AuthorizationCodeErrorException(String statusCode, HttpStatus httpStatus, String message) {
+        super(statusCode, httpStatus, message);
+    }
+
+    public AuthorizationCodeErrorException() {
+        this("", HttpStatus.BAD_REQUEST, "올바르지 않은 Authorization Code 입니다.");
+    }
+}
---
+++ Server/src/main/java/ForMZ/Server/global/oauth/SocialTypeNotFoundException.java
@@ -0,0 +1,14 @@
+package ForMZ.Server.global.oauth;
+
+import ForMZ.Server.global.exception.ApplicationException;
+import org.springframework.http.HttpStatus;
+
+public class SocialTypeNotFoundException extends ApplicationException {
+    public SocialTypeNotFoundException(String statusCode, HttpStatus httpStatus, String message) {
+        super(statusCode, httpStatus, message);
+    }
+
+    public SocialTypeNotFoundException() {
+        this("", HttpStatus.NOT_FOUND, "지원하지 소셜 로그인입니다.");
+    }
+}
