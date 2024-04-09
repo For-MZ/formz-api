67164d856f704bfc759f3d8bd825852339f97eab
Park Jae-Min <dhkdhkdhkdn@gmail.com>
2024-04-10 03:45:16 +0900

    Feat : Jwt 인가 필터 Principal 객체 주입 구현

---
+++ Server/src/main/java/ForMZ/Server/global/security/AuthorizationFilter.java
@@ -0,0 +1,44 @@
+package ForMZ.Server.global.security;
+
+import ForMZ.Server.global.jwt.JwtProvider;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+public class AuthorizationFilter extends OncePerRequestFilter {
+    private static final String AUTHORIZATION_HEADER = "Authorization";
+    private static final String BEARER_PREFIX = "Bearer ";
+    private final JwtProvider jwtProvider;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String authorizationHeader = request.getHeader(AUTHORIZATION_HEADER);
+
+        if(authorizationHeader != null){
+            setAuthPrincipal(getToken(authorizationHeader));
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String getToken(String authorizationHeader) {
+        return authorizationHeader.substring(BEARER_PREFIX.length());
+    }
+
+    private void setAuthPrincipal(String token) {
+        AuthUser authUser = AuthUser.createAuthUser(jwtProvider.getUserId(token));
+        Authentication authentication =
+                    new UsernamePasswordAuthenticationToken(authUser, null);
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+    }
+}
