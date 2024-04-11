e02aacdff5b566636e45209c26442eae69f44dba
Park Jae-Min <dhkdhkdhkdn@gmail.com>
2024-04-11 23:33:27 +0900

    Feat : OAuth Login Controller 구현

--- Server/src/main/java/ForMZ/Server/domain/user/controller/UserController.java
+++ Server/src/main/java/ForMZ/Server/domain/user/controller/UserController.java
@@ -1,6 +1,6 @@
 package ForMZ.Server.domain.user.controller;
 
-import ForMZ.Server.domain.user.constant.UserConstant;
+import ForMZ.Server.domain.user.dto.LoginRes;
 import ForMZ.Server.domain.user.dto.MailReq;
 import ForMZ.Server.domain.user.dto.MailRes;
 import ForMZ.Server.domain.user.service.MailSenderService;
@@ -12,10 +12,7 @@ import ForMZ.Server.domain.user.dto.UserReq;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import static ForMZ.Server.domain.user.constant.UserConstant.AuthResponseMessage.*;
 
@@ -54,5 +51,11 @@ public class UserController {
         MailRes res = mailSenderService.joinEmail(mailReq.getEmail());
         return ResponseEntity.status(HttpStatus.OK)
                 .body(ResponseDto.create(HttpStatus.OK.value(), MAIL_SEND_SUCCESS.getMessage(), res));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> loginOAuth(@RequestParam("target") String target, @RequestParam("code") String code) {
+        LoginRes loginRes = userService.loginOAuth(target, code);
+        return ResponseEntity.status(HttpStatus.OK).body(ResponseDto.create(HttpStatus.OK.value(), LOGIN_USER_SUCCESS.getMessage(), loginRes));
     }
 }
