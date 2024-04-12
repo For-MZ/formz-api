fc5586ad639bb5f5c37a44ecad70edd015edff3a
Park Jae-Min <dhkdhkdhkdn@gmail.com>
2024-04-12 18:23:02 +0900

    Refactor : Security Uri 인가 변경

--- Server/src/main/java/ForMZ/Server/global/config/SecurityConfig.java
+++ Server/src/main/java/ForMZ/Server/global/config/SecurityConfig.java
@@ -6,17 +6,14 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
-@EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
@@ -35,15 +32,10 @@ public class SecurityConfig {
                 .addFilterBefore(new AuthorizationFilter(jwtProvider), UsernamePasswordAuthenticationFilter.class);
 
         http
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers(
-                                new AntPathRequestMatcher("/login"),
-                                new AntPathRequestMatcher("/email")
-                        ).permitAll()
-                        .requestMatchers(
-                                new AntPathRequestMatcher("/my-page")
-                        ).hasRole("USER")
-                        .anyRequest().authenticated());
+                .authorizeHttpRequests(auth ->
+                        auth
+                                .requestMatchers("/api/**").authenticated()
+                                .anyRequest().permitAll());
 
         return http.build();
     }
