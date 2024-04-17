14af814448bad165fd4289e8215578082aebde44
Park Jae-Min <dhkdhkdhkdn@gmail.com>
2024-04-17 18:19:58 +0900

    Test : 쿠키 테스트 코드 작성

---
+++ Server/src/test/java/ForMZ/Server/global/cookie/CookieUtilTest.java
@@ -0,0 +1,37 @@
+package ForMZ.Server.global.cookie;
+
+import jakarta.servlet.http.HttpServletResponse;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.mock.web.MockHttpServletResponse;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(MockitoExtension.class)
+class CookieUtilTest {
+
+    @InjectMocks
+    CookieUtil cookieUtil;
+
+    @Test
+    @DisplayName("Cookie 설정 - 정상")
+    void setCookieTest() {
+        // given
+        HttpServletResponse response = new MockHttpServletResponse();
+        String refreshToken = "refresh";
+
+        // when
+        cookieUtil.setRefreshTokenInCookie(response, refreshToken);
+
+        // then
+        String setCookie = response.getHeader("Set-Cookie");
+        assertThat(setCookie).isNotNull();
+        assertThat(setCookie.indexOf("HttpOnly")).isNotEqualTo(-1);
+        assertThat(setCookie.indexOf("Secure")).isNotEqualTo(-1);
+        assertThat(setCookie.indexOf("SameSite=NONE")).isNotEqualTo(-1);
+        assertThat(setCookie.indexOf("Path=/refresh")).isNotEqualTo(-1);
+    }
+}
