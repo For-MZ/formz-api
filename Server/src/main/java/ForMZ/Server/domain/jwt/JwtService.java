c26c704b903e47f369709e57d05252257549519f
Park Jae-Min <dhkdhkdhkdn@gmail.com>
2024-04-19 02:12:23 +0900

    Feat : AccessToken 재발급 구현

--- Server/src/main/java/ForMZ/Server/domain/jwt/JwtService.java
+++ Server/src/main/java/ForMZ/Server/domain/jwt/JwtService.java
@@ -1,5 +1,6 @@
 package ForMZ.Server.domain.jwt;
 
+import ForMZ.Server.domain.jwt.exception.NotFoundRefreshTokenException;
 import org.springframework.transaction.annotation.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Service;
 public class JwtService {
 
     private final JwtFactory jwtFactory;
+    private final JwtProvider jwtProvider;
     private final JwtProperty jwtProperty;
     private final JwtRepository jwtRepository;
 
@@ -17,5 +19,11 @@ public class JwtService {
         RefreshToken refreshToken = jwtRepository.findByUserId(userId)
                 .orElse(jwtRepository.save(RefreshToken.toEntity(jwtFactory.createRefreshToken(), userId, jwtProperty.getRefreshExpiration())));
         return new JwtToken(jwtFactory.createAccessToken(userId), refreshToken.getValue());
+    }
+
+    public String reIssueAccessToken(String token) {
+        jwtProvider.verifyRefreshToken(token);
+        RefreshToken refreshToken = jwtRepository.findById(token).orElseThrow(NotFoundRefreshTokenException::new);
+        return jwtFactory.createAccessToken(refreshToken.getUserId());
     }
 }
