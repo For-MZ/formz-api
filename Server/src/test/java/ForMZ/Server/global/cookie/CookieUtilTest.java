73ca344ef3a076a9cf6eb6283d9064932cbd9256
Park Jae-Min <dhkdhkdhkdn@gmail.com>
2024-04-18 21:11:27 +0900

    Test : 쿠키 삭제 테스트 코드 작성

--- Server/src/test/java/ForMZ/Server/global/cookie/CookieUtilTest.java
+++ Server/src/test/java/ForMZ/Server/global/cookie/CookieUtilTest.java
@@ -34,4 +34,19 @@ class CookieUtilTest {
         assertThat(setCookie.indexOf("SameSite=NONE")).isNotEqualTo(-1);
         assertThat(setCookie.indexOf("Path=/refresh")).isNotEqualTo(-1);
     }
+
+    @Test
+    @DisplayName("Cookie 삭제 - 정상")
+    void deleteCookieTest() {
+        // given
+        HttpServletResponse response = new MockHttpServletResponse();
+
+        // when
+        cookieUtil.deleteRefreshTokenInCookie(response);
+
+        // then
+        String setCookie = response.getHeader("Set-Cookie");
+        assertThat(setCookie).isNotNull();
+        assertThat(setCookie.indexOf("Max-Age=0")).isNotEqualTo(-1);
+    }
 }
