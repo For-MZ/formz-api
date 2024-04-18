b6521ca384f2ba29d6db45b4b885fdb5db9ecc0f
Park Jae-Min <dhkdhkdhkdn@gmail.com>
2024-04-18 21:00:43 +0900

    Refactor : RefreshToken 생성 조건 수정

--- Server/src/main/java/ForMZ/Server/domain/jwt/JwtService.java
+++ Server/src/main/java/ForMZ/Server/domain/jwt/JwtService.java
@@ -1,6 +1,6 @@
 package ForMZ.Server.domain.jwt;
 
-import jakarta.transaction.Transactional;
+import org.springframework.transaction.annotation.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -9,11 +9,13 @@ import org.springframework.stereotype.Service;
 public class JwtService {
 
     private final JwtFactory jwtFactory;
+    private final JwtProperty jwtProperty;
     private final JwtRepository jwtRepository;
 
     @Transactional
     public JwtToken createJwtToken(long userId) {
-        RefreshToken refreshToken = jwtRepository.save(RefreshToken.toEntity(jwtFactory.createRefreshToken(), userId));
+        RefreshToken refreshToken = jwtRepository.findByUserId(userId)
+                .orElse(jwtRepository.save(RefreshToken.toEntity(jwtFactory.createRefreshToken(), userId, jwtProperty.getRefreshExpiration())));
         return new JwtToken(jwtFactory.createAccessToken(userId), refreshToken.getValue());
     }
 }
